d44d99fc13c5ed94d890a71a7433286686cc6af1
Shyam <pindoria.shyam@gmail.com>
2017-05-31 11:51:43 +0930

    Added panels in MainFrame

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,11 +4,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.awt.CardLayout;
+import javax.swing.border.EmptyBorder;
 
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
-
+	private MedicationPanel panelMedication;
+	private HistoryPanel panelHistory;
+	private ContactsPanel panelContacts;
+	private UploadPanel panelUpload;
+	private HelpPanel panelHelp;
+	private SettingsPanel panelSettings;
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -39,5 +48,33 @@ public class MainFrame extends JFrame {
 		MenuPanel panelMenu = new MenuPanel();
 		contentPane.add(panelMenu, BorderLayout.WEST);
 		
+		JPanel panelBody = createBodyPanel();
+		contentPane.add(panelBody, BorderLayout.CENTER);
+	}
+	
+	private JPanel createBodyPanel() {
+		JPanel panelBody = new JPanel();
+		panelBody.setBorder(new EmptyBorder(0, 12, 0, 0));
+		panelBody.setLayout(new CardLayout(0, 0));
+		
+		this.panelMedication = new MedicationPanel();
+		panelBody.add(this.panelMedication);
+		
+		this.panelHistory = new HistoryPanel();
+		panelBody.add(this.panelHistory);
+		
+		this.panelContacts = new ContactsPanel();
+		panelBody.add(this.panelContacts);
+		
+		this.panelUpload = new UploadPanel();
+		panelBody.add(this.panelUpload);
+		
+		this.panelHelp = new HelpPanel();
+		panelBody.add(this.panelHelp);
+		
+		this.panelSettings = new SettingsPanel();
+		panelBody.add(this.panelSettings);
+		
+		return panelBody;
 	}
 }
